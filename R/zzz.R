f1d989990abd682faa18e3bbe689636c837b1f59
Jian <Jian@jli-fujitsu>
2014-09-11 01:04:50 +0800

    copy from R-forge

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+# TODO: Add comment
+# 
+# Author: jli
+###############################################################################
+
+.onAttach <- function(libname, pkgname ){
+	packageStartupMessage( paste("# Rweibo Version:", packageDescription("Rweibo", fields = "Version")) )
+}
+
